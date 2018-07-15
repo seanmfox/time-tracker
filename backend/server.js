b7c0119390bbbccf9a436ebe619f62a642ba235c
Sean Fox <geekypghrunner@gmail.com>
2018-07-15 15:44:40 -0400

    fix: Correct access of static assets

--- backend/server.js
+++ backend/server.js
@@ -189,12 +189,18 @@ app.use('/api', router);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join('client/build', 'index.html'));
   });
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // // Handle React routing, return all requests to React app
+  // app.get('*', function(req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
 }
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
