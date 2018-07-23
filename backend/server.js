a0e8e3f3408782682bd8448137dad886057dca4a
Sean Fox <geekypghrunner@gmail.com>
2018-07-23 00:19:15 -0400

    fix: COrrect path for server

--- backend/server.js
+++ backend/server.js
@@ -141,12 +141,11 @@ app.use('/api', router);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  console.log(__dirname)
-  app.use( express.static( `${__dirname}/../build` ) );
+  app.use( express.static( `${__dirname}/../client/build` ) );
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   })
   
   // app.use(express.static(path.join(__dirname, 'client/build')));
